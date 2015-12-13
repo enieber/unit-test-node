2cc2f022fb76d8ac80f4a72f4ccc3015114976ef
Enieber <eniebercunha@gmail.com>
2015-12-13 05:14:52 -0200

    [add] new test commented

--- src/RomanInArabic.js
+++ src/RomanInArabic.js
@@ -1,24 +1,30 @@
-function Romans() {}
+(function(exp) {
+    'use strict';
 
-Romans.convert  = function(number){    
-    switch (number) {
-	case 'I':
-	    return 1;    
-	case 'V':
-	    return 5;
-	case 'X':
-	    return 10;
-	case 'L':
-	    return 50;
-	case 'C':
-	    return 100;
-	case 'D':
-	    return 500;
-	case 'M':
-	    return 1000;
-	default:
-	    return 0;
+    function convert(number){    
+	switch (number) {
+	    case 'I':
+		return 1;  
+	   case 'II':
+	     return 2;
+	    case 'V':
+		return 5;
+	   case 'X':
+		return 10;
+	    case 'L':
+		return 50;
+	    case 'C':
+		return 100;
+	    case 'D':
+		return 500;
+	    case 'M':
+		return 1000;
+	    default:
+		return 0;   
+
+	}
     }
- }
+      
+    exp.convert = convert || {};
 
- exports.Romans = Romans;
+})(exports);
--- test/RomanInArabicSpec.js
+++ test/RomanInArabicSpec.js
@@ -3,12 +3,12 @@
 // Algoritm minor in left are substract 
  
 var should = require('should'),
-    Roman = require('../src/RomanInArabic').Romans;
+    Roman = require('../src/RomanInArabic');
 
  
  describe('Convert Numbers Romans in Arabic', function() {
  
-     describe('Convert Symbol unic', function(){
+     describe('Convert single Symbol', function(){
 
 	it('Convert Symbol I in 1', function(){
 	   Roman.convert('I').should.equal(1);
@@ -19,4 +19,15 @@ var should = require('should'),
 	});
 
      });
+
+    describe('Convert double Symbol', function(){
+	
+	it('Convert Symbol II in 2', function(){
+	    Roman.convert('II').should.equal(2);
+	});
+
+	//it('Convert Symbol XX in 10', function(){
+	  //  Roman.convert('XX').should.equal(10);
+	//});
+    });
 });
